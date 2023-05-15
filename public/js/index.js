66806b7f0d2775e982bbdcd575722cbc6b5dedac
Abdelrhman A. Elnhas <elnhasdesigner@gmail.com>
2023-05-15 19:08:50 +0300

    compress trial

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,3 @@
-var bar = document.getElementsByClassName("prgB");
-let section = document.getElementById("about");
-var value = document.querySelector(".prgV");
-let sectionEnd = document.getElementById("services");
-window.onscroll = function () {
-  if (
-    window.scrollY >= section.offsetTop - 100 &&
-    window.scrollY <= sectionEnd.offsetTop
-  ) {
-    for (let x = 0; x < bar.length; x++) {
-      var changeB = bar[x].getAttribute("data-width");
-      bar[x].style.width = changeB;
-      bar[x].style.animation = "fill 3s ease-in-out forwards";
-    }
-  } else {
-    for (let x = 0; x < bar.length; x++) {
-      bar[x].style.width = 0;
-      bar[x].style.animation = "none";
-    }
-  }
-};
-
 document.onkeydown = function (e) {
   if (event.keyCode == 123) {
     return false;
@@ -50,5 +28,27 @@ document.onkeydown = function (e) {
   }
   if (e.ctrlKey && e.keyCode == "P".charCodeAt(0)) {
     return false;
+  }
+};
+
+var bar = document.getElementsByClassName("prgB");
+let section = document.getElementById("about");
+var value = document.querySelector(".prgV");
+let sectionEnd = document.getElementById("services");
+window.onscroll = function () {
+  if (
+    window.scrollY >= section.offsetTop - 100 &&
+    window.scrollY <= sectionEnd.offsetTop
+  ) {
+    for (let x = 0; x < bar.length; x++) {
+      var changeB = bar[x].getAttribute("data-width");
+      bar[x].style.width = changeB;
+      bar[x].style.animation = "fill 3s ease-in-out forwards";
+    }
+  } else {
+    for (let x = 0; x < bar.length; x++) {
+      bar[x].style.width = 0;
+      bar[x].style.animation = "none";
+    }
   }
 };
